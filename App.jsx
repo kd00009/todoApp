2437614a2cf8c4acd2092c9373cb9696d137ec08
karan disoja <disojakaran@gmail.com>
2024-07-15 21:59:36 +0530

    login ui added

--- App.jsx
+++ App.jsx
@@ -1,18 +1,29 @@
-// App.js
-import React from 'react';
-import { StatusBar } from 'react-native';
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { NavigationContainer } from "@react-navigation/native";
+import HomeScreen from "./src/screen/HomeScreen";
+import LoginScreen from "./src/screen/LoginScreen";
+import SignupScreen from "./src/screen/SignupScreen";
 
-import AppNavigator from './src/navigations/AppNavigator';
-import { colors } from './src/constants/theme';
+const Stack = createNativeStackNavigator();
 
 const App = () => {
   return (
-    <>
-      {/* Set status bar color to black */}
-      <StatusBar backgroundColor={colors.header_bg} barStyle="light-content" />
-      <AppNavigator />
-    </>
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{
+          headerShown: false,
+        }}
+      >
+        <Stack.Screen name={"HOME"} component={HomeScreen} />
+        <Stack.Screen name={"LOGIN"} component={LoginScreen} />
+        <Stack.Screen name="SIGNUP" component={SignupScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 };
 
 export default App;
+
+const styles = StyleSheet.create({});
